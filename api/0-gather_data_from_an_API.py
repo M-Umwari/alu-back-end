bc00bcc89e64958aa5bdafc25eb925e713fc3bc1
M-Umwari <m.umwari@alustudent.com>
2023-02-23 12:39:39 +0200

    Gather info👌

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-"""For a given EMPLOYEE_NAME ID, returns information about
+#!/usr/bin/python3
+"""For a given employee ID, returns information about
 their TODO list progress"""
 
 import requests
 import sys
+
 if __name__ == "__main__":
 
     userId = sys.argv[1]
-    EMPLOYEE_NAME = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                         .format(userId))
 
-    name = EMPLOYEE_NAME.json().get('name')
+    name = user.json().get('name')
 
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
     totalTasks = 0
     completed = 0
 
     for task in todos.json():
-        if task.get('EMPLOYEE_NAMEId') == int(userId):
+        if task.get('userId') == int(userId):
             totalTasks += 1
             if task.get('completed'):
                 completed += 1
 
-    print('EMPLOYEE_NAME {} is done with tasks({}/{}):'
+    print('Employee {} is done with tasks({}/{}):'
           .format(name, completed, totalTasks))
 
     print('\n'.join(["\t " + task.get('title') for task in todos.json()
-          if task.get('EMPLOYEE_NAMEId') == int(userId) and task.get('completed')]))
-
+          if task.get('userId') == int(userId) and task.get('completed')]))
